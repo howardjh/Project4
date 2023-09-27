b9dc8b4eddc089ca771fea196772b874024e927a
Jack Howard <howardjh@iu.edu>
2023-09-26 23:32:07 -0400

    Done

--- app/src/main/java/com/example/project3/FragmentQuestions.kt
+++ app/src/main/java/com/example/project3/FragmentQuestions.kt
@@ -1,7 +1,9 @@
 package com.example.project3
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,7 +18,9 @@ class FragmentQuestions : Fragment() {
     private var ref1 = 0
     private var ref2 = 0
     private lateinit var ansText: EditText
-    //private val args = FragmentQuestionsArgs.fromBundle(requireArguments())
+    private lateinit var correctSound: MediaPlayer
+    private lateinit var wrongSound: MediaPlayer
+    // private val args = FragmentQuestionsArgs.fromBundle(requireArguments())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,10 +31,19 @@ class FragmentQuestions : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        correctSound = MediaPlayer.create(context, R.raw.correct)
+        wrongSound = MediaPlayer.create(context, R.raw.incorrect)
+        //correctSound.prepare()
+        //wrongSound.prepare()
+        correctSound.stop()
+        wrongSound.stop()
+        correctSound.prepare()
+        wrongSound.prepare()
         val view = inflater.inflate(R.layout.fragment_questions, container, false)
         val difficulty = FragmentQuestionsArgs.fromBundle(requireArguments()).difficulty
         val operation = FragmentQuestionsArgs.fromBundle(requireArguments()).operation
         val numOfQuestions = FragmentQuestionsArgs.fromBundle(requireArguments()).numOfQuestions
+        val retNumQuestion = numOfQuestions
         ansText = view.findViewById<EditText>(R.id.editTextNumber)
         var tempNum = 1
         val done = view.findViewById<Button>(R.id.bDone)
@@ -38,22 +51,44 @@ class FragmentQuestions : Fragment() {
         Log.e("operation", operation)
         Log.e("Number of Questions", numOfQuestions.toString())
         updateQuestion(operation, difficulty)
+        Log.e("ref1", ref1.toString())
+        Log.e("ref2", ref2.toString())
         done.setOnClickListener {
             val userAnswer = ansText?.text.toString().toIntOrNull()
-            if (userAnswer == calculateCorrectAnswer(ref1, ref2, operation)) numCorrect++
+            if (userAnswer == calculateCorrectAnswer(ref1, ref2, operation)) {
+                numCorrect++
+                val correctToast = Toast.makeText(context, "Correct. Good Work!", Toast.LENGTH_SHORT)
+                correctToast.show()
+                if (wrongSound.isPlaying){
+                    wrongSound.stop()
+                    wrongSound.prepare()
+                }
+                if(correctSound.currentPosition != -1) correctSound.start()
+            }
+            else{
+                val incorrectToast = Toast.makeText(context, "Wrong", Toast.LENGTH_SHORT)
+                incorrectToast.show()
+                if (correctSound.isPlaying) {
+                    correctSound.stop()
+                    correctSound.prepare()
+                }
+                if(wrongSound.currentPosition != -1) wrongSound.start()
+            }
             Log.d("Input Answer", userAnswer.toString())
             Log.d("Actual Answer", calculateCorrectAnswer(ref1, ref2, operation).toString())
             if (tempNum <= numOfQuestions) {
                 tempNum++
                 updateQuestion(operation, difficulty)
-            } else {
+            }
+            //if (tempNum == numOfQuestions + 1) tempNum++
+            else {
                 // Navigate to next fragment
-                val bundle1 = Bundle()
+                val args = Bundle()
                 Log.e("Total Correct", numCorrect.toString())
-                bundle1.putInt("totalCorrect", numCorrect)
-                bundle1.putInt("numOfQuestions", numOfQuestions)
-                view.findNavController()
-                    .navigate(R.id.action_fragement_questions_to_fragmentResult, bundle1)
+                Log.e("Total Questions before Fragment switch", retNumQuestion.toString())
+                args.putInt("pastNumOfQuestions", retNumQuestion)
+                args.putInt("pastNumCorrect", numCorrect)
+                view.findNavController().navigate(R.id.action_fragment_questions_to_fragment_welcome, args)
             }
         }
         return view
@@ -77,17 +112,23 @@ class FragmentQuestions : Fragment() {
             if(operator == "-"){
                 val operand1 = (1 until 10).random()
                 val operand2 = (1 until operand1).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             if(operator == "/"){
                 val operand1 = generateNonPrime(1 until 10)
                 val factors = getFactors(operand1)
                 val operand2 = factors.random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             else{
                 val operand1 = (1 until 10).random()
                 val operand2 = (1 until 10).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
         }
@@ -95,17 +136,23 @@ class FragmentQuestions : Fragment() {
             if(operator == "-"){
                 val operand1 = (1 until 25).random()
                 val operand2 = (1 until operand1).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             if(operator == "/"){
                 val operand1 = generateNonPrime(1 until 25)
                 val factors = getFactors(operand1)
                 val operand2 = factors.random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             else{
                 val operand1 = (1 until 25).random()
                 val operand2 = (1 until 25).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
         }
@@ -113,17 +160,23 @@ class FragmentQuestions : Fragment() {
             if(operator == "-"){
                 val operand1 = (1 until 50).random()
                 val operand2 = (1 until operand1).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             if(operator == "/"){
                 val operand1 = generateNonPrime(1 until 10)
                 val factors = getFactors(operand1)
                 val operand2 = factors.random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
             else{
                 val operand1 = (1 until 50).random()
                 val operand2 = (1 until 50).random()
+                Log.e("operand1", operand1.toString())
+                Log.e("operand2", operand2.toString())
                 return Pair(operand1, operand2)
             }
         }
@@ -173,6 +226,8 @@ class FragmentQuestions : Fragment() {
         }
         return factors
     }
+
+
 
 }
 
--- app/src/main/java/com/example/project3/FragmentWelcome.kt
+++ app/src/main/java/com/example/project3/FragmentWelcome.kt
@@ -1,6 +1,7 @@
 package com.example.project3
 
 import android.os.Bundle
+import com.example.project3.R
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,18 +10,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import java.math.*
+import kotlin.properties.Delegates
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private lateinit var btnNumbers: List<Button>
 private lateinit var btnDifficulty: List<Button>
+private var pastTotal by Delegates.notNull<Int>()
+private var pastTotalCorrect by Delegates.notNull<Int>()
 private var operation = ""
 private var difficulty = ""
 private var numOfQuestions = 0
-private var x = 1
-
 
 
 /**
@@ -34,7 +37,16 @@ class FragmentWelcome : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("Welcome Fragment entered", "Welcome!!!!")
+        val args = requireArguments()
+        Log.d("BundleKeys", args.keySet().toString())
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        pastTotal = args.getInt("pastNumOfQuestions", 0)
+        pastTotalCorrect = args.getInt("pastNumCorrect", 0)
+        //FragmentWelcomeArgs.fromBundle(requireArguments()).pastNumCorrect ?: 0
+        Log.e("Past Total Number of Questions WelcomeFragment", pastTotal.toString())
+        Log.e("Past Total Number of Correct Answers", pastTotalCorrect.toString())
+        // args.getInt("pastNumCorrect") ?: 0
         val easyButton = view.findViewById<RadioButton>(R.id.bEasy);
         easyButton.isChecked = true
         difficulty = "Easy"
@@ -42,6 +54,7 @@ class FragmentWelcome : Fragment() {
         addButton.isChecked = true
         operation = "+"
         val startButton = view.findViewById<Button>(R.id.bStart)
+        val lastResult = view.findViewById<TextView>(R.id.tvLastResult)
         val plusButton = view.findViewById<Button>(R.id.bPlus)
         val minusButton = view.findViewById<Button>(R.id.bMinus)
         val questionNum = view.findViewById<TextView>(R.id.tvProblemNumber)
@@ -49,6 +62,19 @@ class FragmentWelcome : Fragment() {
             view.findViewById(R.id.bMultiplication),view.findViewById(R.id.bDivision))
         btnDifficulty = listOf(view.findViewById(R.id.bEasy), view.findViewById(R.id.bMedium),
             view.findViewById(R.id.bHard))
+
+        if (pastTotal == 0 && pastTotalCorrect == 0){ }
+        else{
+            val scorePercent = pastTotalCorrect.toFloat() / pastTotal.toFloat()
+            Log.e("Correct Percentage", scorePercent.toString())
+            if(scorePercent < .80){
+                lastResult.setTextColor(ContextCompat.getColor(requireContext(), R.color.Red))
+                lastResult.text = "You got $pastTotalCorrect out of $pastTotal correct. You need more practice."
+            } else{
+                lastResult.text = "You got $pastTotalCorrect out of $pastTotal correct. Good Work!"
+            }
+        }
+
         btnNumbers.forEach{ btn ->
             btn.setOnClickListener{
                 if(btn.id == R.id.bAddition){
@@ -110,7 +136,7 @@ class FragmentWelcome : Fragment() {
                 bundle.putString("difficulty", difficulty)
                 bundle.putString("operation", operation)
                 bundle.putInt("numOfQuestions", numOfQuestions)
-                view.findNavController().navigate(R.id.action_fragment_welcome_to_fragement_questions,bundle)
+                view.findNavController().navigate(R.id.action_fragment_welcome_to_fragment_questions,bundle)
             }
         }
 
